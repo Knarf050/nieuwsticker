5d8b7ec653fe618c9b3d3df9ae73821d4054824c
Knarf050 <f.alserda@gmail.com>
2026-02-13 15:16:13 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -7,11 +7,9 @@ export default async function handler(req, res) {
   
   const SOURCES = [
     { name: 'NOS', url: 'https://feeds.nos.nl/nosnieuwsalgemeen', color: '#FF6B00' },
-    { name: 'AD', url: 'https://www.ad.nl/rss.xml', color: '#E30613' },
     { name: 'Telegraaf', url: 'https://www.telegraaf.nl/rss', color: '#003DA5' },
     { name: 'NU.nl', url: 'https://www.nu.nl/rss/Algemeen', color: '#00A0DC' },
     { name: 'NRC', url: 'https://www.nrc.nl/rss/', color: '#000000' },
-    { name: 'RTL Nieuws', url: 'https://www.rtlnieuws.nl/rss.xml', color: '#FF6600' }
   ];
 
   try {
